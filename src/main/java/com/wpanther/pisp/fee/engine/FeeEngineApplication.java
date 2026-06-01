b1385939df3629c43b81c8972c229b0353e9a7a2
Weerachat Wongsawat <rabbit_roger@yahoo.com>
2026-06-01 15:33:21 +0700

    feat: bootstrap fee-engine service

---
+++ src/main/java/com/wpanther/pisp/fee/engine/FeeEngineApplication.java
@@ -0,0 +1,11 @@
+package com.wpanther.pisp.fee.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeeEngineApplication.class, args);
+    }
+}
