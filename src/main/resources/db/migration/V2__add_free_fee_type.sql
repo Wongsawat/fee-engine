5ceb33c33d5f099b3d0b1e34fd99830dff06f998
Weerachat Wongsawat <rabbit_roger@yahoo.com>
2026-06-02 17:27:04 +0700

    feat: split FREE constraint migration into NOT VALID + VALIDATE for zero-downtime

--- src/main/resources/db/migration/V2__add_free_fee_type.sql
+++ src/main/resources/db/migration/V2__add_free_fee_type.sql
@@ -1,4 +1,5 @@
 ALTER TABLE fee_rules
     ADD CONSTRAINT chk_free_no_amount
         CHECK (fee_type != 'FREE' OR
-               (flat_amount IS NULL AND percentage IS NULL AND tiers IS NULL));
+               (flat_amount IS NULL AND percentage IS NULL AND tiers IS NULL))
+        NOT VALID;
---
+++ src/main/resources/db/migration/V3__validate_free_fee_constraint.sql
@@ -0,0 +1,2 @@
+ALTER TABLE fee_rules
+    VALIDATE CONSTRAINT chk_free_no_amount;
