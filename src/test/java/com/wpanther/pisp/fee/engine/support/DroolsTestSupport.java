e384b6e5f9abe1b8f06bd1d393905cb9b94d5821
Weerachat Wongsawat <weerachatw@generali.co.th>
2026-06-09 10:42:34 +0700

    test(tier): update DroolsTestSupport to delegate to FeeSessionRunner

--- src/test/java/com/wpanther/pisp/fee/engine/support/DroolsTestSupport.java
+++ src/test/java/com/wpanther/pisp/fee/engine/support/DroolsTestSupport.java
@@ -1,34 +1,25 @@
 package com.wpanther.pisp.fee.engine.support;
 
+import com.wpanther.pisp.fee.engine.application.service.FeeSessionRunner;
 import com.wpanther.pisp.fee.engine.domain.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class DroolsTestSupport {
 
     protected KieContainer kieContainer;
+    private FeeSessionRunner sessionRunner;
 
     @BeforeEach
     void setupKie() {
         kieContainer = KieServices.Factory.get().getKieClasspathContainer();
+        sessionRunner = new FeeSessionRunner(kieContainer);
     }
 
     protected List<Charge> fireRules(FeeRequest request, List<FeeRule> rules) {
-        KieSession session = kieContainer.newKieSession("FeeSession");
-        try {
-            List<Charge> charges = new ArrayList<>();
-            session.setGlobal("charges", charges);
-            session.insert(request);
-            rules.forEach(session::insert);
-            session.fireAllRules();
-            return List.copyOf(charges);
-        } finally {
-            session.dispose();
-        }
+        return sessionRunner.run(request, rules);
     }
 }
